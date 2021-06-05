303af63e2e1b26f09c754fba5b40076b5987f206
akanksha-sahu <62078695+akanksha-sahu@users.noreply.github.com>
2021-06-05 13:15:20 +0530

    Add files via upload

---
+++ Note1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note1() {
+  return (
+    <div className="note">
+      <h1>Python and Machine Learning</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha.Various topics
+        related to Python and Machine Lerning were covered.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Note1;
---
+++ Note2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note2() {
+  return (
+    <div className="note">
+      <h1>Web Dev with Node.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha.Various topics
+        related to backend web dev were covered.
+      </p>
+    </div>
+  );
+}
+
+export default Note2;
